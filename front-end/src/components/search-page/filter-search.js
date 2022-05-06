e3b3664bc4c9ecc151f21ad5661c03ab6463fca3
aedanmc <aedanmc@cs.washington.edu>
2022-05-05 20:48:41 -0700

    Create skeletons of static landing page components

---
+++ front-end/src/components/search-page/filter-search.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+
+export default function FilterSearch() {
+    <div>
+        <FormControl>
+            <InputLabel></InputLabel>
+            <Select>
+
+            </Select>
+            <InputLabel></InputLabel>
+            <Select>
+
+            </Select>
+            <InputLabel></InputLabel>
+            <Select>
+
+            </Select>
+        </FormControl>
+    </div>
+}
+
