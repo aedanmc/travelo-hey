12698b6d324f5141b8247c02915be09a4b066220
mik3ycodez <mjharris0114@gmail.com>
2022-05-09 23:58:08 -0700

    removed extraneous call to non-existing file

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import SearchPage from './components/search-page/SearchPage';
+// import SingleResult from "./components/general/SingleResult";
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // TODO: set up nested routing for different top level pages
 function App() {
   return (
     <div className="App">
-      <SearchPage />
     </div>
   );
 }
