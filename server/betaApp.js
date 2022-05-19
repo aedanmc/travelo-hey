895c2950f17a3a8e6cfa1cf2c43f235488890155
camila-christensen <camilyo@cs.washington.edu>
2022-05-18 20:47:57 -0700

    Updated white spaces

--- server/betaApp.js
+++ server/betaApp.js
@@ -93,7 +93,7 @@
             }
         } catch (error) {
             res.type("text").status(500)
-                .send(error);
+              .send(error);
         }
     });
 
@@ -163,13 +163,13 @@
     async function writeReviews(review_params) {
         const db = await instance.getDBConnection();
         const query_insert = "INSERT INTO reviews" +
-            "(userID, placeID, createdAt, inclusiveLanguages, neutralRestrooms, queerBusinessPromotion, " +
-            "accessibility, queerSignage, safety, recommendedBusiness, review) " +
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+          "(userID, placeID, createdAt, inclusiveLanguages, neutralRestrooms, queerBusinessPromotion, " +
+          "accessibility, queerSignage, safety, recommendedBusiness, review) " +
+          "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         await db.run(query_insert, review_params);
         const query_select = "SELECT * FROM reviews " +
-            "WHERE placeID = ? ORDER BY createdAt ASC";
+                            "WHERE placeID = ? ORDER BY createdAt ASC";
 
         const placeID = review_params[1];
         const rows = await db.all(query_select, [placeID]);
