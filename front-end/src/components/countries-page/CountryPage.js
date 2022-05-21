410013122495e64330832076a4e4af8a589d8df9
aedanmc <aedanmc@cs.washington.edu>
2022-05-21 11:26:46 -0700

    Add CountryPage initial static version, add SampleCountry to populate with example data

--- front-end/src/components/countries-page/CountryPage.js
+++ front-end/src/components/countries-page/CountryPage.js
@@ -1,25 +1,35 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
-import axios from 'axios';
-import {
-  Routes,
-  Route,
-  Link,
-  Outlet,
-} from 'react-router-dom';
-import PropTypes from 'prop-types';
+// import axios from 'axios';
+// import {
+//   Routes,
+//   Route,
+//   Link,
+//   Outlet,
+// } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 import SingleResult from '../general/SingleResult';
-import FilterSearch from './FilterSearch';
-import LocationPage from '../locations-page/LocationPage';
+import sampleData from './SampleCountry';
 
 function CountriesPage() {
   // TODO: Add data fetching --> avoid data race with fix from https://maxrozen.com/race-conditions-fetching-data-react-with-useeffect
   // TODO: Add state management with hooks
   // TODO: fill in static layout
   // TODO if time: add styling
+
+  const country = sampleData;
   return (
     <Container width="100%" sx={{ margin: 2 }}>
+      <Stack container="true" spacing={2} alignItems="center" direction="column" sx={{ margin: 2 }}>
+        <SingleResult
+          firstString={country.name}
+          secondString={country.safetyScore}
+          thirdString={country.lgbtqSafetyIndex}
+        />
+      </Stack>
     </Container>
   );
 }
+
+export default CountriesPage;
