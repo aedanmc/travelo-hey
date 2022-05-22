58f1943768730b581a05c5978d554e65cd355e09
aedanmc <aedanmc@cs.washington.edu>
2022-05-21 19:09:14 -0700

    Implement initial static version of country data display

--- front-end/src/components/countries-page/CountryPage.js
+++ front-end/src/components/countries-page/CountryPage.js
@@ -1,78 +1,57 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Container from '@mui/material/Container';
-// import axios from 'axios';
-// import {
-//   Routes,
-//   Route,
-//   Link,
-//   Outlet,
-// } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-import SearchIcon from '@mui/icons-material/Search';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Button from '@mui/material/Button';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import SingleResult from '../general/SingleResult';
-import sampleData from './SampleCountry';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
 
-function CountriesPage() {
-  // TODO: Add data fetching --> avoid data race with fix from https://maxrozen.com/race-conditions-fetching-data-react-with-useeffect
-  // TODO: Add state management with hooks
-  // TODO: fill in static layout
-  // TODO if time: add styling
-
-  const [continent, setContinent] = React.useState('North America');
-
-  const handleContinentChange = (event) => {
-    setContinent(event.target.value);
-  };
-
-  const handleApplyFilters = () => {
-    // TODO: add data fetching here in response to event
-  };
-
-  const country = sampleData;
+export default function CountryPage({
+  image, name,
+  safetyRank, safetyScore, countryNotes,
+  countryWorkers, countryDiscProtection,
+  countryCrimViolence, countryAdoption,
+  countryGoodPlace, countryTransgender,
+  countrySameSex, countryPropaganda,
+  countryMarriage }) {
   return (
-    <Container width="100%" sx={{ margin: 2 }}>
-      <Stack container="true" spacing={3} alignItems="center" direction="row" sx={{ margin: 3 }}>
-        <FormControl>
-          <FormLabel id="radio-buttons-group-label">Continent</FormLabel>
-          <RadioGroup
-            row
-            aria-labelledby="continent-radio-group"
-            name="row-radio-buttons-group"
-            onChange={handleContinentChange}
-            value={continent}
-          >
-            <FormControlLabel value="Asia" control={<Radio />} label="Asia" />
-            <FormControlLabel value="Africa" control={<Radio />} label="Africa" />
-            <FormControlLabel value="Oceania" control={<Radio />} label="Oceania" />
-            <FormControlLabel value="Europe" control={<Radio />} label="Europe" />
-            <FormControlLabel value="North America" control={<Radio />} label="North America" />
-            <FormControlLabel value="South America" control={<Radio />} label="South America" />
-          </RadioGroup>
-        </FormControl>
-        <Button
-          variant="outlined"
-          startIcon={<SearchIcon />}
-          onClick={handleApplyFilters}
-        >
-          Search
-        </Button>
-      </Stack>
-      <Stack container="true" spacing={2} alignItems="center" direction="column" sx={{ margin: 2 }}>
-        <SingleResult
-          firstString={country.name}
-          secondString={country.safetyScore}
-          thirdString={country.lgbtqSafetyIndex}
-        />
-      </Stack>
-    </Container>
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <img alt={name} src={image} />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography color="text.secondary" component="div" data-testid="country-name">{name}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-safety">{`Country safety score: ${safetyScore}`}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-rank">{`Overall Safety Rank:${safetyRank}`}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-same-gender-marriage">{`Same-gender marriage: ${countryMarriage}`}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography color="text.secondary" component="div" data-testid="country-notes">{countryNotes}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-disc">{countryDiscProtection}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-workers">{countryWorkers}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-good">{`Percentage of residents who say ${name} is a good place to live: ${countryGoodPlace}`}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-transgender">{countryTransgender}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-propaganda">{countryPropaganda}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-same-sex-legal">{`Legality of same sex relationships: ${countrySameSex ? 'unknown' : countrySameSex}`}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-violence">{countryCrimViolence}</Typography>
+          <Typography color="text.secondary" component="div" data-testid="country-adopt">{`Level of adoption recognition: ${countryAdoption}`}</Typography>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
-export default CountriesPage;
+CountryPage.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  countryWorkers: PropTypes.string.isRequired,
+  countryPropaganda: PropTypes.string.isRequired,
+  countryDiscProtection: PropTypes.string.isRequired,
+  countryCrimViolence: PropTypes.string.isRequired,
+  countryAdoption: PropTypes.string.isRequired,
+  countryGoodPlace: PropTypes.string.isRequired,
+  countryTransgender: PropTypes.string.isRequired,
+  countrySameSex: PropTypes.string.isRequired,
+  safetyRank: PropTypes.string.isRequired,
+  safetyScore: PropTypes.string.isRequired,
+  countryNotes: PropTypes.string.isRequired,
+  countryMarriage: PropTypes.string.isRequired,
+};
---
+++ front-end/src/components/countries-page/SearchCountry.js
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
+// import axios from 'axios';
+// import {
+//   Routes,
+//   Route,
+//   Link,
+//   Outlet,
+// } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+import SearchIcon from '@mui/icons-material/Search';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import SingleResult from '../general/SingleResult';
+import sampleData from './SampleCountry';
+
+function SearchCountry() {
+  // TODO: Add data fetching --> avoid data race with fix from https://maxrozen.com/race-conditions-fetching-data-react-with-useeffect
+  // TODO: Add state management with hooks
+  // TODO: fill in static layout
+  // TODO if time: add styling
+
+  const [continent, setContinent] = React.useState('North America');
+
+  const handleContinentChange = (event) => {
+    setContinent(event.target.value);
+  };
+
+  const handleApplyFilters = () => {
+    // TODO: add data fetching here in response to event
+  };
+
+  const country = sampleData;
+  return (
+    <Container width="100%" sx={{ margin: 2 }}>
+      <Stack container="true" spacing={3} alignItems="center" direction="row" sx={{ margin: 3 }}>
+        <FormControl>
+          <FormLabel id="radio-buttons-group-label">Continent</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="continent-radio-group"
+            name="row-radio-buttons-group"
+            onChange={handleContinentChange}
+            value={continent}
+          >
+            <FormControlLabel value="Asia" control={<Radio />} label="Asia" />
+            <FormControlLabel value="Africa" control={<Radio />} label="Africa" />
+            <FormControlLabel value="Oceania" control={<Radio />} label="Oceania" />
+            <FormControlLabel value="Europe" control={<Radio />} label="Europe" />
+            <FormControlLabel value="North America" control={<Radio />} label="North America" />
+            <FormControlLabel value="South America" control={<Radio />} label="South America" />
+          </RadioGroup>
+        </FormControl>
+        <Button
+          variant="outlined"
+          startIcon={<SearchIcon />}
+          onClick={handleApplyFilters}
+        >
+          Search
+        </Button>
+      </Stack>
+      <Stack container="true" spacing={2} alignItems="center" direction="column" sx={{ margin: 2 }}>
+        <SingleResult
+          firstString={country.name}
+          secondString={country.safetyScore}
+          thirdString={country.lgbtqSafetyIndex}
+        />
+      </Stack>
+    </Container>
+  );
+}
+
+export default SearchCountry;
