098a99af3f8ec6e04f182ae5044a927fe9240a60
aedanmc <aedanmc@cs.washington.edu>
2022-05-24 22:06:52 -0700

    Remove unnecessary imports from SingleReview

--- front-end/src/components/general/SingleReview.js
+++ front-end/src/components/general/SingleReview.js
@@ -4,7 +4,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import Typography from '@mui/material/Typography';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 // TODO: ensure styling is correct
