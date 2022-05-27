dc0e7f7c6e7590da34146c3a22ea25fa99de80b2
mzuniga-1833381 <mzuniga@cs.washington.edu>
2022-05-27 02:12:35 -0700

    axios code - need to account for new review submission page changes now

--- front-end/src/components/review-submission-page/ReviewSubmissionPage.js
+++ front-end/src/components/review-submission-page/ReviewSubmissionPage.js
@@ -1,24 +1,50 @@
 import * as React from 'react';
 import './ReviewSubmissionPage.css';
 import { useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ReviewInputText from './ReviewInputText';
 import ReviewInputRating from './ReviewInputRating';
-// import { useParams } from 'react-router-dom';
 
 export default function ReviewSubmissionPage() {
-  // const params = useParams();
-  // const id = params.place_id;
+  const params = useParams();
+  const id = params.place_id;
   const defaultValues = {
     textValue: '',
     ratingValue: 0,
   };
   const methods = useForm({ defaultValues });
   const { handleSubmit, control } = methods;
-  // TODO: need to set up sending the submitted content to the back-end
-  const onSubmit = (data) => console.log(data);
+
+  async function postReview() {
+    try {
+      await axios.post('http://localhost:8080/reviews/new', { userID: 1,
+        placeID: id,
+        inclusiveLanguages: null,
+        neutralRestrooms: null,
+        queerBusinessPromotion: null,
+        accessibility: null,
+        queerSignage: null,
+        safety: null,
+        recommendedBusiness: null,
+        review: null })
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const onSubmit = () => {
+    React.useEffect(() => {
+      postReview();
+    }, []);
+  };
 
   return (
     <Paper>
