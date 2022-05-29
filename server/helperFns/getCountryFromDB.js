e6f623996ba307c74b4f0a1ade556bfae9ed40fd
mik3ycodez <mjharris0114@gmail.com>
2022-05-28 21:17:54 -0700

    added copyright contribution in class comment, updated class comment

--- server/helperFns/getCountryFromDB.js
+++ server/helperFns/getCountryFromDB.js
@@ -1,3 +1,19 @@
+/**
+ * CSE 403 Spring 2022
+ *
+ * Copyright ©2022 All rights reserved.
+ *      - Aedan McCall          aedanmc (at) uw.edu
+ *      - Alex Zúñiga           mzuniga (at) uw.edu
+ *      - Camila Christensen    camilyo (at) cs.washington.edu
+ *      - Matt Broom            matty162 (at) uw.edu
+ *      - Michael Harris        micha06 (at) uw.edu
+ *
+ * Helper function to retrieve country information from the database.
+ *
+ * Requires:
+ *   - ../db.connection:     Used to establish connection to local database
+ */
+
 const instance = require('../db.connection');
 
 /**
--- server/helperFns/getLatLongFromDB.js
+++ server/helperFns/getLatLongFromDB.js
@@ -1,3 +1,19 @@
+/**
+ * CSE 403 Spring 2022
+ *
+ * Copyright ©2022 All rights reserved.
+ *      - Aedan McCall          aedanmc (at) uw.edu
+ *      - Alex Zúñiga           mzuniga (at) uw.edu
+ *      - Camila Christensen    camilyo (at) cs.washington.edu
+ *      - Matt Broom            matty162 (at) uw.edu
+ *      - Michael Harris        micha06 (at) uw.edu
+ *
+ * Helper function to retrieve the latitude and longitude values for a given city.
+ *
+ * Requires:
+ *   - ../db.connection:     Used to establish connection to local database
+ */
+
 const instance = require('../db.connection');
 
 /**
