08e2de9823fb4b27a2588747f473a966de075b0d
aedanmc <aedanmc@cs.washington.edu>
2022-05-28 21:18:59 -0700

    Remove commented code from CountrySearchPage-- <Grid> works fine and <Stack> is not necessary.

--- front-end/src/components/countries-search-page/CountrySearchPage.js
+++ front-end/src/components/countries-search-page/CountrySearchPage.js
@@ -1,10 +1,7 @@
-/* eslint-disable max-len */
 import * as React from 'react';
-// import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import axios from 'axios';
 import { Outlet } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
 // import sampleData from './SampleCountry';
 import CountrySearchBar from './CountryFilterSearch';
@@ -42,30 +39,6 @@ function CountrySearchPage() {
     <>
       <CountrySearchBar countries={countriesList} onClick={setCountryDisplay} />
       <Container sx={{ marginTop: 3, padding: 2, width: '100%' }}>
-        {/* TODO: figure out how to style Stack so that it takes up the whole width of the screen */}
-        {/* <Stack container spacing={2} alignItems="center" direction="column" sx={{ margin: 2, width: '100%' }}>
-          {countryDisplay?.map((item) => (
-            <Link key={item.name} to={`/country/${item.name}`}>
-              <SingleResult
-                image={traveloHeyLogo}
-                firstString={item.name}
-                secondString={`Safety score: ${item.safetyScore}`}
-              />
-            </Link>
-          ))}
-        </Stack> */}
-        {/* <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          {countryDisplay?.map((item) => (
-            <Grid item xs={12}>
-              <Link key={item.name} to={`/country/${item.name}`}>
-                <SingleResult
-                  image={traveloHeyLogo}
-                  firstString={item.name}
-                  secondString={`Safety score: ${item.safetyScore}`}
-                />
-              </Link>
-            </Grid>
-          ))} */}
         <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           {countryDisplay?.map((item) => (
             <CountryDetails
